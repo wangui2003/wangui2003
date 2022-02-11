05948124b47b761449b9ee3c9ee9e21f426cd95b
wangui2003 <99465604+wangui2003@users.noreply.github.com>
2022-02-11 09:42:56 +0300

    Add files via upload

---
+++ wangui.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import streamlit as st
+st.header('''simple web app''')
+st.write('''*my app*''')
+value=st.slider('vail')
+st.write(value,"square is",value*value)
+
+
+# In[ ]:
+
+
+
+
